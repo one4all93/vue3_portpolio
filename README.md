0470a3fb1e3c7e6717e86d5b534df4b34c8a54bd
seodj93 <seodj93@kakao.com>
2025-04-22 02:06:01 +0900

    vue3_portpolio :: 초기 세팅

--- README.md
+++ README.md
@@ -3,3 +3,17 @@
 This template should help get you started developing with Vue 3 in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
 
 Learn more about IDE Support for Vue in the [Vue Docs Scaling up Guide](https://vuejs.org/guide/scaling-up/tooling.html#ide-support).
+
+// main 기능 정리
+- 네이버맵 API 연동
+- 공공데이터 포털 데이터 연계
+- 
+
+// 주요 수정/추가 및 이슈
+< 2025.04.21 >
+- 프로젝트 생성 및 "SyntaxError: Unexpected token '||='" 이슈로 pakage.json :: "type": "module", ← 삭제 **
+- 현업에서는 vue2 + webpack 조합을 사용했으나 -> 현 프로젝트에서는 vue3 + vite 조합을 사용함
+
+< 2025.04. >
+- 열린데이터 광장 > 서울시 공공도서관 데이터.json example폴더에 추가 (일단 개발용으로 json파일 사용하고, api연계는 추후에 진행 예정**)
+- 
---
+++ src/components/NaverMap.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="map" style="width:1400px;height:1000px;"></div>
+</template>
+
+<script>
+export default {
+    name: 'NaverMap',
+    data(){
+        return{
+            map : null,
+            lat : 37.3595704,
+            lng : 127.105399,
+            zoom : 13,
+        }
+    },
+    created(){
+        console.log('NaverMap created');
+        //this.initMap();
+    },
+    computed: {
+        mapOptions(){
+            return {
+                center: new naver.maps.LatLng(this.lat, this.lng),
+                zoom: this.zoom
+            }
+        }
+    },
+    mounted(){
+        console.log('NaverMap mounted');
+
+        this.map = new naver.maps.Map('map', this.mapOptions);
+        console.log('NaverMap mounted map', this.map);
+    },
+    methods: {
+        initMap() {
+            map = new naver.maps.Map('map', {
+                center: new naver.maps.LatLng(37.3595704, 127.105399),
+                zoom: 10
+            });
+        }
+    }
+}
+</script>
+
+<style>
+    #map {
+    width: 100%;
+    height: 500px;
+    }
+</style>
